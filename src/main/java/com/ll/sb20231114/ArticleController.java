e189b8a2a1d07131be29e46ad8665e460a331883
gayoi <dbs9476@naver.com>
2023-11-14 17:28:31 +0900

    step2

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,37 +6,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @Controller
 public class ArticleController {
-    private Article [] articles = new Article[3];
-    private
+
 
     @GetMapping("/article/write")
-    String showWrite(){
+    String showWrite() {
         return "article/write"; // aritcle/write에 있는 폴더가 필요하다
     }
 
-
-
-    @GetMapping("/article/getLastArticle")
+    @GetMapping("/article/doWrite")
     @ResponseBody
-    Map<String,Object>doWrite(
+    String doWrite(
             String title,
             String body
-    ){
-        Article article = new Article(1,title,body);
-
-        Map<String, Object> rs = new HashMap<>();
-        rs.put("msg","1번 게시물이 작성되었습니다");
-        rs.put("data", article);
-
-        return rs;
+    ) {
+        return "게시물이 작성되었습니다.";
     }
 
+
+
 }
+
 
 @AllArgsConstructor // 모든 필드를 argument로 하는 ..?
 @Getter // <- 얘가 필요하다
@@ -45,4 +36,6 @@ class Article {
     private String title;
     private String body;
 }
+
+
 
