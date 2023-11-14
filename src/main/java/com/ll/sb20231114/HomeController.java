36190508b93b1cb8465ccb6af4674afe898631af
gayoi <dbs9476@naver.com>
2023-11-14 10:55:46 +0900

    calc5

--- src/main/java/com/ll/sb20231114/HomeController.java
+++ src/main/java/com/ll/sb20231114/HomeController.java
@@ -2,6 +2,7 @@ package com.ll.sb20231114;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -14,8 +15,48 @@ public class HomeController {
     }
 
     @GetMapping("/about")
-    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의
+    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
     String showAbout() {
         return "개발자 커뮤니티";
+    }
+
+    @GetMapping("/calc")
+    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
+    String showCalc(int a, int b) {
+        return "계산 결과 : %d".formatted(a+b);
+    }
+
+   @GetMapping("/calc2")
+    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
+    String showCalc2(Integer a,Integer b) {
+        return "a : %d" + a + ", b : "+b;
+    }
+    @GetMapping("/calc3")
+    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
+    String showCalc3(
+            @RequestParam(defaultValue = "0" ) int a,
+            @RequestParam(defaultValue = "0" ) int b
+    ) {
+
+        return "계산 결과 : %d".formatted(a+b);
+    }
+
+    @GetMapping("/calc4")
+    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
+    String showCalc4(
+            @RequestParam(defaultValue = "0" ) double a,
+            @RequestParam(defaultValue = "0" ) double b
+    ) {
+        return "계산 결과 : %f".formatted(a+b);
+    }
+
+    @GetMapping("/calc5")
+    @ResponseBody // 이 함수의 리턴값을 그대로 브라우저에 전송하라는 의미
+    String showCalc5(
+            @RequestParam(defaultValue = "0" ) String a,
+            @RequestParam(defaultValue = "0" ) String b
+    ) {
+
+        return "계산 결과 : %s".formatted(a+b);
     }
 }
