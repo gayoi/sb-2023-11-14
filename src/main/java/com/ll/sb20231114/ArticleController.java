c2395e6f0825c4e0afbc3ef89c7fd86d51698804
gayoi <dbs9476@naver.com>
2023-11-14 17:46:33 +0900

    step4

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 public class ArticleController {
 
@@ -17,20 +20,23 @@ public class ArticleController {
 
     @GetMapping("/article/doWrite")
     @ResponseBody
-    String doWrite(
+    Map<String, Object> doWrite(
             String title,
             String body
     ) {
-        return "게시물이 작성되었습니다.";
+        Map<String, Object> rs = new HashMap<>();
+        rs.put("msg","1번 게시물이 작성되었습니다");
+        Article article = new Article(1,title,body);
+        rs.put("data", article);
+
+        return rs;
     }
-
-
 
 }
 
 
 @AllArgsConstructor // 모든 필드를 argument로 하는 ..?
-@Getter // <- 얘가 필요하다
+@Getter // <- 얘가 필요하다 //getid,gettitle등등
 class Article {
     private long id;
     private String title;
