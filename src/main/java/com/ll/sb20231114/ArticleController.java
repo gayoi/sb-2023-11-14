7f8363d46db387499f14da2aad8a40256a155187
gayoi <dbs9476@naver.com>
2023-11-16 19:23:10 +0900

    step7

--- src/main/java/com/ll/sb20231114/ArticleController.java
+++ src/main/java/com/ll/sb20231114/ArticleController.java
@@ -6,14 +6,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class ArticleController {
     //private Article lastArticle; 새 게시물을 작성하면 기존꺼는 삭제 되기 때문에 배열로
-    private Article[] articles = new Article[3]; // 리모콘 3개를 넣을 수 있다 0,1,2
-    private int articleLastIndex = -1; //게시물이 하나도 없으면 오류가 난다
+    private List<Article> articles = new ArrayList<>(); // 3개 이상차면 오류나니까 이부분을 list 로 바꿔준다
 
     @GetMapping("/article/write")
     String showWrite() {
@@ -27,25 +28,25 @@ public class ArticleController {
             String title,
             String body
     ) {
-        Article article = new Article(articleLastIndex+2, title,   body); // article <-함수 안에서 정의 된 변수는 지역변수이다
+        Article article = new Article(articles.size()+1, title,   body); // article <-함수 안에서 정의 된 변수는 지역변수이다
 
         Map<String, Object> rs = new HashMap<>();
         rs.put("msg","%d번 게시물이 작성되었습니다".formatted(article.getId()));
         rs.put("data", article);
 
-        articles[++articleLastIndex] = article;
+        articles.add(article);
 
         return rs;
     }
     @GetMapping("/article/getLastArticle")
     @ResponseBody
     Article getLastArticle(){
-        return articles[articleLastIndex];
+        return articles.getLast();
     }
 
     @GetMapping("/article/getArticles")
     @ResponseBody
-    Article[] getArticles(){
+    List<Article> getArticles(){
         return articles;
     }
 
